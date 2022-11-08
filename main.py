aa995b14c680c2711571a9f41136b4b847954143
Peter <prichieri16@gmail.com>
2022-11-08 14:21:37 -0500

    Fixed the coordinate issue. Need to apply the functions to all FOVs

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ import statistics
 CELL_MASK_DATA = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellLabels\CellLabels_F004.tif"
 COMPOSITE_IMAGE = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellComposite\CellComposite_F004.jpg"
 METADATA = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\C4_R5042_S1_metadata_file.csv"
-
+FOV_GLOBAL_X = int(-4972.22222222222)
+FOV_GLOBAL_Y = int(144450)
 
 ''' Get cell data as numpy array, convert to binary, and return. '''
 def read_mask(path_to_file):
     with tifffile.Timer(f'\nReading cell masks\n'):
         raw_data = tifffile.imread(path_to_file)
         print('... completed in ', end='')
-    # Also have to flip is around to be [x,y] instead of [y,x]
+    # Have to flip is around to be [x,y] instead of [y,x]
     return np.transpose(np.clip(raw_data,0,1),(1,0))
 
 ''' Data only exists as composite .jpg. DAPI signal should be the B channel of the RGB data. '''
@@ -35,6 +36,7 @@ def drop_weak_signal(arr, lower_limit):
     arr[arr < lower_limit] = 0
     return arr
 
+''' Read in metadata file and return data for the current fov'''
 def read_metadata(path_to_file, fov = 4):
     meta = pd.read_csv(path_to_file)
     meta_fov = meta.loc[meta["fov"] == fov]
@@ -44,27 +46,28 @@ def read_transcripts(path_to_file):
     pass
 
 '''Subset numpy array to only data corresponding to a certain Cell_ID and return that data'''
-def get_pixels_for_cell(cell_mask,cell_id,metadata):
+def get_pixels_for_cell(cell_mask,cell_id,metadata,fov_x,fov_y, max_local_y):
     row = metadata.loc[metadata["cell_ID"]==cell_id]
-    center_x = row["CenterX_local_px"].values[0]
-    center_y = row["CenterY_local_px"].values[0]
+    local_center_x = int(row["CenterX_global_px"].values[0] - fov_x)
+    local_center_y = int(max_local_y - (row["CenterY_global_px"].values[0] - fov_y))
     width = row["Width"].values[0]
     height = row["Height"].values[0]
+
+    if cell_id == 3583:
+        print(f"\nCID 3583 cX is {local_center_x} and cY is {local_center_y}\n")
     # if cell_id == 3585:
-        # print(row.loc[:,["cell_ID","Area","CenterX_local_px","Width","Height"]])
-    return cell_mask[center_x-(width//2):center_x+(width//2),center_y-(height//2):center_y+(height//2)]
+    #     print(row.loc[:,["cell_ID","Area","CenterX_local_px","Width","Height"]])
+    return cell_mask[local_center_x-(width//2):local_center_x+(width//2),local_center_y-(height//2):local_center_y+(height//2)]
 
-def mean_for_all_cells(cell_data, metadata):
-    print(f"\npost-threshold shape is {cell_data.shape}, max is {np.max(cell_data)}, min is {np.min(cell_data)}")
-
+''' Return a dictionary of the mean DAPI value for all cells
+    Key: a cell ID          value: Maan of DAPI counts per pixel for that cell'''
+def mean_for_all_cells(cell_data, metadata, fov_x,fov_y, max_local_y):
     cell_lookup = {}
     for cell_id in metadata["cell_ID"]:
-        current = get_pixels_for_cell(cell_data,cell_id,metadata)
-
+        current = get_pixels_for_cell(cell_data,cell_id,metadata,fov_x,fov_y,max_local_y)
         cell_lookup[cell_id] = np.mean(current)
-        # print(cell_id)
         # if cell_id >3570:
-        #     print(f"shape is {current.shape}, max is {np.max(current)}, min is {np.min(current)}\n{current}")
+        #     print(f"\nCID {cell_id} shape is {current.shape}, max is {np.max(current)}, min is {np.min(current)}\n{current} mean is {cell_lookup[cell_id]}")
     return cell_lookup
 
 def main():
@@ -72,22 +75,17 @@ def main():
     print(f"\ncell_mask shape is {cell_mask.shape}, max is {np.max(cell_mask)}, min is {np.min(cell_mask)}")
 
     dapi_only = extract_dapi_signal(COMPOSITE_IMAGE)
-
+    max_X = dapi_only.shape[0]; max_Y = dapi_only.shape[1]
     print(f"\ndapi shape is {dapi_only.shape}, max is {np.max(dapi_only)}, min is {np.min(dapi_only)}")
 
     # Now make quantitative counts for each DAPI pixel
-    dapi_cells = drop_weak_signal(cell_mask * dapi_only,20)
-
-    print(f"DAPI cell mask shape is {dapi_cells.shape}")
-
+    dapi_cells = drop_weak_signal(cell_mask * dapi_only,15)
     metadata = read_metadata(METADATA)
 
-    # print(metadata.columns)
-    dapi_means = mean_for_all_cells(dapi_cells,metadata)
-
+    dapi_means = mean_for_all_cells(dapi_cells,metadata, FOV_GLOBAL_X, FOV_GLOBAL_Y, max_Y)
+    print(f"max value for dapi means is {max(dapi_means.values())}")
     for id in range(3500,3585):
         print(dapi_means[id])
-    print(f"max value for dapi means is {max(dapi_means.values())}")
 
 if __name__ == "__main__":
     main()
