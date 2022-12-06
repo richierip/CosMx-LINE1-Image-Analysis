fb795de555b503f6d3404e0fe8009eb8af82226d
Peter <prichieri16@gmail.com>
2022-12-06 09:12:11 -0500

    Forgot to commit for a while...a lot of changes since then. About to try to go back and use CosMx compartment label and cell label masks to more efficiently get nuclei estimates.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 Compare nuclear intensity vs LINE1 transcripts per cell, run correlation / regression
-
+    This file compiles the data for plotting
 Peter Richieri 
 MGH Ting Lab 
 11/7/22
@@ -9,24 +9,39 @@ import numpy as np
 import tifffile # for cell masks
 from matplotlib import image # for DAPI .jpg
 import pandas as pd
-import statistics
+import time
+import os
 
-CELL_MASK_DATA = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellLabels\CellLabels_F004.tif"
-COMPOSITE_IMAGE = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellComposite\CellComposite_F004.jpg"
-METADATA = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\C4_R5042_S1_metadata_file.csv"
-FOV_GLOBAL_X = int(-4972.22222222222)
-FOV_GLOBAL_Y = int(144450)
+# For roundness metrics
+
+import math
+import matplotlib.pyplot as plt
+from skimage.draw import ellipse
+from skimage.measure import label, regionprops, regionprops_table
+from skimage.transform import rotate
+
+# CELL_MASK_DATA = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellLabels\CellLabels_F004.tif"
+# COMPOSITE_IMAGE = r"C:\Users\prich\Desktop\Projects\MGH\CosMx_Data\RawData\CellComposite\CellComposite_F004.jpg"
+METADATA = r"..\RawData\C4_R5042_S1_metadata_file.csv"
+FOV_POSITIONS = r"..\RawData\C4_R5042_S1_fov_positions_file.csv"
+TRANSCRIPTS = r"..\RawData\C4_R5042_S1_exprMat_file.csv"
+MIN_DAPI_INTENSITY_THRESHOLD = 15
+MIN_DAPI_AREA = 30 # In pixels
+MIN_PANCK_THRESHOLD = 900
+# FOV_GLOBAL_X = int(-4972.22222222222)
+# FOV_GLOBAL_Y = int(144450)
+RESULTS_FILE = r"..\DAPI_Intensity_by_Cell.csv"
 
 ''' Get cell data as numpy array, convert to binary, and return. '''
 def read_mask(path_to_file):
-    with tifffile.Timer(f'\nReading cell masks\n'):
-        raw_data = tifffile.imread(path_to_file)
-        print('... completed in ', end='')
+    print(f'Reading cell masks',end="   ")
+    raw_data = tifffile.imread(path_to_file)
     # Have to flip is around to be [x,y] instead of [y,x]
     return np.transpose(np.clip(raw_data,0,1),(1,0))
 
 ''' Data only exists as composite .jpg. DAPI signal should be the B channel of the RGB data. '''
 def extract_dapi_signal(path_to_file):
+    print(f'Reading composite',end="   ")
     raw_data = image.imread(path_to_file)
     # Also have to flip is around to be [x,y] instead of [y,x]
     return np.transpose(raw_data[:,:,2],(1,0))
@@ -42,9 +57,6 @@ def read_metadata(path_to_file, fov = 4):
     meta_fov = meta.loc[meta["fov"] == fov]
     return meta_fov
 
-def read_transcripts(path_to_file):
-    pass
-
 '''Subset numpy array to only data corresponding to a certain Cell_ID and return that data'''
 def get_pixels_for_cell(cell_mask,cell_id,metadata,fov_x,fov_y, max_local_y):
     row = metadata.loc[metadata["cell_ID"]==cell_id]
@@ -52,40 +64,145 @@ def get_pixels_for_cell(cell_mask,cell_id,metadata,fov_x,fov_y, max_local_y):
     local_center_y = int(max_local_y - (row["CenterY_global_px"].values[0] - fov_y))
     width = row["Width"].values[0]
     height = row["Height"].values[0]
-
-    if cell_id == 3583:
-        print(f"\nCID 3583 cX is {local_center_x} and cY is {local_center_y}\n")
-    # if cell_id == 3585:
-    #     print(row.loc[:,["cell_ID","Area","CenterX_local_px","Width","Height"]])
+    # if cell_id == 778:
+    #     print(row.loc[:,["cell_ID","Area","CenterX_local_px","CenterY_local_px","Width","Height"]])
+    #     print(f"X and Y {local_center_x} and {local_center_y}")
     return cell_mask[local_center_x-(width//2):local_center_x+(width//2),local_center_y-(height//2):local_center_y+(height//2)]
 
 ''' Return a dictionary of the mean DAPI value for all cells
-    Key: a cell ID          value: Maan of DAPI counts per pixel for that cell'''
-def mean_for_all_cells(cell_data, metadata, fov_x,fov_y, max_local_y):
-    cell_lookup = {}
+    Key: a cell ID          value: Mean of DAPI counts per pixel for that cell'''
+def mean_for_all_cells(cell_data, cell_lookup, other_params, metadata, fov, fov_x,fov_y, max_local_y):
     for cell_id in metadata["cell_ID"]:
+        # print(f"My inputs are CID {cell_id} ,fov {fov}, fovX {fov_x}, fovY {fov_y}")
         current = get_pixels_for_cell(cell_data,cell_id,metadata,fov_x,fov_y,max_local_y)
-        cell_lookup[cell_id] = np.mean(current)
-        # if cell_id >3570:
-        #     print(f"\nCID {cell_id} shape is {current.shape}, max is {np.max(current)}, min is {np.min(current)}\n{current} mean is {cell_lookup[cell_id]}")
-    return cell_lookup
+        
+        # dump zeroes
+        current = np.ndarray.flatten(current)
+        current = current[current !=0]
+        if len(current)<MIN_DAPI_AREA:
+            continue
+        cell_lookup[f"{fov}_{cell_id}"] = np.mean(current)
+        other_params[f"{fov}_{cell_id}_area"] = len(current)
+        # if cell_id >2250:   
+        #     print(f"\nCID {cell_id} shape is {current.shape}, max is {np.max(current)}, min is {np.min(current)}\n{current} mean is {cell_lookup[str(fov)+'_'+str(cell_id)]}")
+    return cell_lookup, other_params
 
-def main():
-    cell_mask = read_mask(CELL_MASK_DATA)
-    print(f"\ncell_mask shape is {cell_mask.shape}, max is {np.max(cell_mask)}, min is {np.min(cell_mask)}")
+def get_coordinate_conversions(fov):
+    meta = pd.read_csv(FOV_POSITIONS)
+    meta_fov = meta.loc[meta["fov"] == fov]
+    return int(meta_fov["x_global_px"].values[0]), int(meta_fov["y_global_px"].values[0])
 
-    dapi_only = extract_dapi_signal(COMPOSITE_IMAGE)
+def compute_roundness(dapi_cells):
+    print(f"\nComputing roundness...")
+    label_img = label(dapi_cells)
+    regions = regionprops(label_img)
+
+    fig, ax = plt.subplots()
+    ax.imshow(dapi_cells, cmap=plt.cm.gray)
+
+    for props in regions:
+        y0, x0 = props.centroid
+        orientation = props.orientation
+        x1 = x0 + math.cos(orientation) * 0.5 * props.axis_minor_length
+        y1 = y0 - math.sin(orientation) * 0.5 * props.axis_minor_length
+        x2 = x0 - math.sin(orientation) * 0.5 * props.axis_major_length
+        y2 = y0 - math.cos(orientation) * 0.5 * props.axis_major_length
+
+        ax.plot((x0, x1), (y0, y1), '-r', linewidth=2.5)
+        ax.plot((x0, x2), (y0, y2), '-r', linewidth=2.5)
+        ax.plot(x0, y0, '.g', markersize=15)
+
+        minr, minc, maxr, maxc = props.bbox
+        bx = (minc, maxc, maxc, minc, minc)
+        by = (minr, minr, maxr, maxr, minr)
+        ax.plot(bx, by, '-b', linewidth=2.5)
+
+    ax.axis((0, 600, 600, 0))
+    plt.show()
+
+def add_counts_for_fov(cell_dictionary, other_params, fov, cell_mask_data, composite):
+    cell_mask = read_mask(cell_mask_data)
+    # print(f"\ncell_mask shape is {cell_mask.shape}, max is {np.max(cell_mask)}, min is {np.min(cell_mask)}")
+
+    dapi_only = extract_dapi_signal(composite)
     max_X = dapi_only.shape[0]; max_Y = dapi_only.shape[1]
-    print(f"\ndapi shape is {dapi_only.shape}, max is {np.max(dapi_only)}, min is {np.min(dapi_only)}")
+    # print(f"\ndapi shape is {dapi_only.shape}, max is {np.max(dapi_only)}, min is {np.min(dapi_only)}")
 
     # Now make quantitative counts for each DAPI pixel
-    dapi_cells = drop_weak_signal(cell_mask * dapi_only,15)
-    metadata = read_metadata(METADATA)
+    dapi_cells = drop_weak_signal(cell_mask * dapi_only,MIN_DAPI_INTENSITY_THRESHOLD)
 
-    dapi_means = mean_for_all_cells(dapi_cells,metadata, FOV_GLOBAL_X, FOV_GLOBAL_Y, max_Y)
-    print(f"max value for dapi means is {max(dapi_means.values())}")
-    for id in range(3500,3585):
-        print(dapi_means[id])
+    # compute_roundness(dapi_cells)
+
+    metadata = read_metadata(METADATA, fov)
+    fov_global_X, fov_global_Y = get_coordinate_conversions(fov)
+
+    return mean_for_all_cells(dapi_cells,cell_dictionary,other_params, metadata,fov, fov_global_X, fov_global_Y, max_Y)
+
+def add_transcripts(path_to_file, cell_df):
+    transcripts = pd.read_csv(path_to_file)
+
+    # Need this to get PanCK data
+    metadata = pd.read_csv(METADATA) 
+
+    cell_df.insert(5,"Line1_ORF1", "")
+    cell_df.insert(6,"Line1_ORF2", "")
+    cell_df.insert(7,"Line1_Combined", "")
+    cell_df.insert(8,"Mean PanCK","")
+    cell_df.insert(9,"Cancer?","")
+    for index,cell in cell_df.iterrows():
+        cell_tx = transcripts[(transcripts["fov"] == int(cell["fov"])) & (transcripts["cell_ID"]== int(cell["local_cid"]))]
+        panck = metadata.loc[(metadata["fov"] == int(cell["fov"])) & (metadata["cell_ID"]== int(cell["local_cid"])),"Mean.PanCK"].values[0]
+        orf1 = cell_tx["LINE1_ORF1"].values[0]
+        orf2 = cell_tx["LINE1_ORF2"].values[0]
+        cell_df.at[index,"Line1_ORF1"] = orf1
+        cell_df.at[index,"Line1_ORF2"] = orf2
+        cell_df.at[index,"Line1_Combined"] = orf1 + orf2
+        cell_df.at[index,"Mean PanCK"] = panck
+        if panck > MIN_PANCK_THRESHOLD:
+            cell_df.at[index,"Cancer?"] = "Cancer"
+        else:
+            cell_df.at[index,"Cancer?"] = "Not Cancer"
+    return cell_df
+        
+def assemble_df(cell_dict,other_params):
+    df = pd.DataFrame(columns=['global_cid', 'fov', 'local_cid','DAPI Intensity Mean','DAPI Area (px)'])
+    for cid in cell_dict.keys():
+        cell_mean = cell_dict[cid]
+        both = cid.split("_")
+        d = {'global_cid': cid, "fov":both[0],"local_cid":both[1]
+            ,'DAPI Intensity Mean':cell_mean, "DAPI Area (px)": other_params[cid+"_area"]}
+        df = pd.concat([df,pd.DataFrame(data=d,index=[1])], ignore_index=True)
+    return df
+
+def dump_csv(df):
+    df.to_csv(RESULTS_FILE, index=False)
+    return None
+
+def main():
+    # print(f"max value for dapi means is {max(dapi_means.values())}")
+    dapi_means={}
+    other_params={}
+    start = time.time()
+    for root,dirs,files in os.walk("../RawData/CellComposite"):
+        # print(files)
+        for composite in files:
+            cell_mask = os.path.normpath(os.path.join(root,"../CellLabels/CellLabels_" + composite.split("_")[1].rstrip(".jpg") + ".tif"))
+            fov = int(composite.split("_")[1].rstrip(".jpg").lstrip("[F0]"))
+            composite_path = os.path.normpath(os.path.join(root,composite))
+            # print(f"Composite path is {composite_path} and mask is {cell_mask}")
+            print(f"\nWorking on FOV {fov}...",end="   ")
+            dapi_means, other_params = add_counts_for_fov(dapi_means,other_params, fov, cell_mask, os.path.join(root,composite))
+            print("Done.")
+            # if fov ==1: break # for testing
+    end = time.time()
+    print(f"\nTotal runtime: {end-start} seconds.")
+
+    print(f"\nAssembling data for {len(dapi_means.keys())} cells... ", end="   ")
+    output = add_transcripts(TRANSCRIPTS, assemble_df(dapi_means, other_params))
+    print("Done.")
+    print(f"\n Writing to {RESULTS_FILE} ... ", end='   ')
+    dump_csv(output)
+    print("Done.")
 
 if __name__ == "__main__":
     main()
---
+++ plot.py
@@ -0,0 +1,151 @@
+'''
+Compare nuclear intensity vs LINE1 transcripts per cell, run correlation / regression
+    This file takes in a curated data table and makes a plot
+Peter Richieri 
+MGH Ting Lab 
+11/9/22
+'''
+
+import seaborn as sns
+from seaborn import lmplot
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import sys
+
+custom_data_path = r"..\DAPI_Intensity_by_Cell.csv"
+
+def bin_by_dapi(df, norm_by_area=False):
+    def _calc_norm_area(row):
+        return row["DAPI Intensity Mean"] / row["DAPI Area (px)"]
+    # def _calc_percentile(row, colmax):
+    #     return row["DAPI Intensity over Area"] *100 / colmax
+    def _label_decile(row):
+        rank = row["DAPI Intensity over Area Decile"]
+        if rank == 0 or rank == '0':
+            start = str(rank)
+        else:
+            start = str(rank) + '0'
+        end = str(rank+1)+'0'
+        return start + ' to '+ end
+    if norm_by_area:
+        # independent_var = "DAPI Intensity over Area"
+        bin_name = "DAPI_Area_Norm_Bin"
+        df["DAPI Intensity over Area"] = df.apply(lambda row:_calc_norm_area(row), axis=1)
+        df["DAPI Intensity over Area Decile"] = pd.qcut(df["DAPI Intensity over Area"],10, labels=False)
+        #df.apply(lambda row:_calc_percentile(row,df.loc[df['DAPI Intensity over Area'].idxmax()]['DAPI Intensity over Area']), axis=1)
+        # independent_var = "DAPI Intensity over Area Percentile"
+        df["DAPI_Area_Norm_Bin"] = df.apply(lambda row: _label_decile(row),axis=1)
+
+    elif not norm_by_area:
+        independent_var = "DAPI Intensity Mean"
+        bin_name = "DAPI_Bin"
+    
+    # print(f"\nmy iv is {independent_var} and bin name is {bin_name}")
+        df.insert(4,bin_name,"")
+        df.loc[df[independent_var] <15, bin_name] = "0 to 15"
+        df.loc[(15< df[independent_var]) & (df[independent_var] <=20), bin_name] = "15 to 20"
+        df.loc[(20< df[independent_var]) & (df[independent_var] <=30), bin_name] = "20 to 30"
+        df.loc[(30<df[independent_var]) & (df[independent_var] <=40), bin_name] = "30 to 40"
+        df.loc[(40<df[independent_var]) & (df[independent_var] <=50), bin_name] = "40 to 50"
+        df.loc[(50<df[independent_var]) & (df[independent_var] <=60), bin_name] = "50 to 60"
+        df.loc[(60<df[independent_var]) & (df[independent_var] <=70), bin_name] = "60 to 70"
+        df.loc[(70<df[independent_var]) & (df[independent_var] <=80), bin_name] = "70 to 80"
+        df.loc[(80<df[independent_var]) & (df[independent_var] <=90), bin_name] = "80 to 90"
+        df.loc[(90<df[independent_var]) & (df[independent_var] <=101), bin_name] = "90 to 100"
+        if independent_var == "DAPI Intensity Mean":
+            df.loc[df[independent_var] >101, bin_name] = ">100"
+    df.to_csv(custom_data_path, index=False)
+    return df
+
+def scatter(path_to_data, cmd_args):
+    model = cmd_args[1]
+    if len(cmd_args)>2:
+        iv = cmd_args[2]
+        if iv == 'intensity':
+            independent_variable = "DAPI Intensity Mean"
+        elif iv == 'area':
+            independent_variable = "DAPI Intensity over Area"
+        else:
+            print("\nNeed 'intensity' or 'area'")
+            exit(0)
+    def annotate(data, **kws):
+        if model == "linear":
+            r, p = stats.pearsonr(data[independent_variable], data["Line1_Combined"])
+            ax = plt.gca()
+            ax.text(.5, .8, 'Pearson r={:.2f}, p={:.2g}'.format(r, p),transform=ax.transAxes)
+        elif model == "polynomial":
+            r, p = stats.spearmanr(data[independent_variable], data["Line1_Combined"])
+            ax = plt.gca()
+            ax.text(.5, .8, 'Spearman r={:.2f}, p={}'.format(r, p),transform=ax.transAxes)
+        else:
+            print("\nNeed 'linear' or 'polynomial'")
+            exit(0)
+            
+    df = pd.read_csv(path_to_data).dropna() # remove N/As for scipy regression stats
+    cancer_only = df[df["Cancer?"]=="Not Cancer"]
+    if model == "linear":
+        p = lmplot(data=df, x=independent_variable, y="Line1_Combined",
+                    col="Cancer?",  scatter_kws={"s":2})
+    elif model == "polynomial":
+        p = lmplot(data=df, x=independent_variable, y="Line1_Combined",
+                    col="Cancer?", order=3, scatter_kws={"s":2})
+    # sns.lmplot(data=cancer_only, x="DAPI Intensity Mean", y="Line1_Combined")
+    else:
+        print("Bad input")
+        exit(0)
+    p.map_dataframe(annotate)
+    plt.show()
+
+def plot_dapi_bins(pre_process_df, cmd_args):
+    if len(cmd_args) >2:
+        bar_type = cmd_args[2]
+    if len(cmd_args)>3:
+        if cmd_args[3]== 'intensity':
+            independent_var = "DAPI_Bin"
+            title_var = ' DAPI Intensity (binned)'
+        elif cmd_args[3]=='area':
+            independent_var = "DAPI_Area_Norm_Bin"
+            title_var = " DAPI Intensity over Area (percentile rank)"
+    else:
+        bar_type = "mean"
+        independent_var = "DAPI_Bin"
+        title_var = ' DAPI Intensity (binned)'
+
+    # df = bin_by_dapi(pre_process_df)
+    df = pre_process_df
+    if independent_var == "DAPI_Bin":
+        x_order = ["0 to 15","15 to 20","20 to 30","30 to 40","40 to 50","50 to 60","60 to 70","70 to 80","80 to 90","90 to 100",">100"]
+    elif independent_var == "DAPI_Area_Norm_Bin":
+        x_order = ["0 to 10","10 to 20","20 to 30","30 to 40","40 to 50","50 to 60","60 to 70","70 to 80","80 to 90","90 to 100"]
+        
+    
+    l1bins = sns.barplot(data=df, x=independent_var,y="Line1_Combined", errorbar='sd',
+                estimator = bar_type,order = x_order, hue="Cancer?").set(title= bar_type.title()+title_var+' vs ORF1/2 Expression')
+    # axis_flip = sns.barplot(data=df, y=independent_var,x="Line1_Combined", errorbar='sd',
+    #             estimator = bar_type,order = x_order, hue="Cancer?").set(title= 'L1 ORF1/2 Expression vs '+bar_type.title()+title_var)
+    plt.show()
+    l1counts = sns.countplot(data=df, x=independent_var,hue="Cancer?",order=x_order).set(title= 'Cell Counts by '+title_var)
+    
+    # print(f"type of counts is {type(l1counts)} and bins is {type(l1bins)}")
+    
+    # l1counts.bar_label(l1counts.containers[0])
+    plt.show()
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'linear' or sys.argv[1] == 'polynomial':
+            print("Preparing to plot a trendline...")
+            scatter(custom_data_path, sys.argv)
+        elif sys.argv[1] == 'barplot':
+            print("Assembling a bar plot...")
+            plot_dapi_bins(pd.read_csv(custom_data_path).dropna(), sys.argv)
+        else:
+            print("Check your input.")
+    else:
+        print("\nCaclulating bins to append to input dataframe...   ",end='')
+        bin_by_dapi(df = pd.read_csv(custom_data_path).dropna(), norm_by_area=True)
+        print("Done.")
+
+if __name__=='__main__':
+    main()
